2f509d7339010717d81014325a1132df6a855c85
SEH00N <06psh0501@gmail.com>
2024-01-20 23:12:26 +0900

    asd

--- ProjectWak/Assets/01. Scripts/Module/Health.cs
+++ ProjectWak/Assets/01. Scripts/Module/Health.cs
@@ -1,64 +1,66 @@
-using System.Reflection;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class Health : MonoBehaviour
+namespace ProjectWak.Modules
 {
-    // <this, performer, amount>
-    public UnityEvent<Health, GameObject, float> OnHPModifiedEvent = null;
-	public UnityEvent OnDeadEvent = null;
-
-    private float currentHP = 0f;
-    public float CurrentHP => currentHP;
-
-    private float maxHP = 50f;
-    public float MaxHP => maxHP;
-
-    private bool isDead = false;
-    public bool IsDead => isDead;
-
-    public void SetMaxHP(float maxHP)
+    public class Health : MonoBehaviour
     {
-        this.maxHP = maxHP;
-    }
+        // <this, performer, amount>
+        public UnityEvent<Health, GameObject, float> OnHPModifiedEvent = null;
+        public UnityEvent OnDeadEvent = null;
 
-    public void Reset()
-    {
-        isDead = false;
-        currentHP = maxHP;
-    }
+        private float currentHP = 0f;
+        public float CurrentHP => currentHP;
 
-    public void TakeDamage(float damage, GameObject performer)
-    {
-        if(isDead)
-            return;
+        private float maxHP = 50f;
+        public float MaxHP => maxHP;
 
-        ModifyHP(-damage, false);
-        OnHPModifiedEvent?.Invoke(this, performer, -damage);
+        private bool isDead = false;
+        public bool IsDead => isDead;
 
-        if(currentHP <= 0f)
-            OnDie();
-    }
+        public void SetMaxHP(float maxHP)
+        {
+            this.maxHP = maxHP;
+        }
 
-    public void HealHP(float amount, GameObject performer, bool overflow = false)
-    {
-        float modifiedAmount = ModifyHP(amount, overflow);
-        OnHPModifiedEvent?.Invoke(this, performer, modifiedAmount);
-    }
+        public void Reset()
+        {
+            isDead = false;
+            currentHP = maxHP;
+        }
 
-    private float ModifyHP(float amount, bool overflow)
-    {
-        float prevHP = currentHP;
-        currentHP += amount;
-        if(overflow == false)
-            currentHP = Mathf.Clamp(currentHP, 0f, maxHP);
-        
-        return currentHP - prevHP;
-    }
+        public void TakeDamage(float damage, GameObject performer)
+        {
+            if(isDead)
+                return;
 
-    private void OnDie()
-    {
-        isDead = true;
-        OnDeadEvent?.Invoke();
+            ModifyHP(-damage, false);
+            OnHPModifiedEvent?.Invoke(this, performer, -damage);
+
+            if(currentHP <= 0f)
+                OnDie();
+        }
+
+        public void HealHP(float amount, GameObject performer, bool overflow = false)
+        {
+            float modifiedAmount = ModifyHP(amount, overflow);
+            OnHPModifiedEvent?.Invoke(this, performer, modifiedAmount);
+        }
+
+        private float ModifyHP(float amount, bool overflow)
+        {
+            float prevHP = currentHP;
+            currentHP += amount;
+            if(overflow == false)
+                currentHP = Mathf.Clamp(currentHP, 0f, maxHP);
+            
+            return currentHP - prevHP;
+        }
+
+        private void OnDie()
+        {
+            isDead = true;
+            OnDeadEvent?.Invoke();
+        }
     }
 }
